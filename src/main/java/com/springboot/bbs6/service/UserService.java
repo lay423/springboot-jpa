a3607a7ce257e12559123249f8ffede3e76fc717
lay423 <junpang486@gmail.com>
2022-11-17 15:20:48 +0900

    [update] 중복check 기능 추가

--- src/main/java/com/springboot/bbs6/service/UserService.java
+++ src/main/java/com/springboot/bbs6/service/UserService.java
@@ -6,6 +6,7 @@ import com.springboot.bbs6.domain.entity.User;
 import com.springboot.bbs6.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,6 +29,12 @@ public class UserService {
     }
 
     public ResponseDto addUser(RequestDto dto) {
+        List<User> all = userRepository.findAll();
+        for (User user1 : all) {
+            if(user1.getUsername().equals(dto.getUsername())){
+                return new ResponseDto(null, dto.getUsername(), "해당 username은 중복입니다.");
+            }
+        }
         User user = userRepository.save(dto.toEntity());
         return User.of(user);
     }
